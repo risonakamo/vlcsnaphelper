47156dcd238ad93ff79c2b83873ec704cdfe15b9
risonakamo <hmykanae@gmail.com>
2018-02-11 14:02:29 -0500

    img flex fit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var _win;
 
 function main()
 {
-    _win=new BrowserWindow({width:800,height:600,useContentSize:true});
+    _win=new BrowserWindow({width:800,height:600,useContentSize:true,webPreferences:{webSecurity:false}});
 
     _win.loadURL(`${__dirname}/index.html`);
 }
