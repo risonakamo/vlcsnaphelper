8f1cfbab462830cfb3c572c7e861fcee2c08f387
risonakamo <hmykanae@gmail.com>
2018-10-28 22:23:21 -0400

    initial commit setup stuff testing out image flex layout

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app,BrowserWindow}=require("electron");
+
+var _win;
+
+function main()
+{
+    _win=new BrowserWindow({width:850,height:550,useContentSize:true,webPreferences:{webSecurity:false}});
+
+    _win.loadURL(`${__dirname}/mainwindow/mainwindow.html`);
+}
+
+app.on("ready",main);
